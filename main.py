fb7de49d66bcc8c387be8d743176cd9429410337
aman <aman.apk01@gmail.com>
2025-06-02 21:16:12 +0530

    modified main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, jsonify
 import pymongo
 from pymongo import MongoClient
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
+IST = timezone(timedelta(hours=5, minutes=30))
 
 app = Flask(__name__)
 
@@ -10,6 +11,10 @@ client = MongoClient("mongodb://localhost:27017/")
 db = client['gihub-webhook']
 collection = db['github-events']
 
+def insert_into_mongodb(doc):
+    #insert data into mongodb
+    collection.insert_one(doc)
+    return jsonify({"status": "success"}), 200
 
 @app.route('/github', methods=['POST'])
 def github_webhook():
@@ -23,9 +28,10 @@ def github_webhook():
         "action": "PUSH",
         "from_branch": None,
         "to_branch" : payload["ref"].split("/")[-1], 
-        "timestamp" : datetime.now(timezone.utc).strftime('%d %B %Y - %I:%M %p UTC')
+        "timestamp" : datetime.now(IST).strftime('%d %B %Y - %I:%M %p UTC')
         }
-        print(f'{doc["author"]} submitted a pull request from {doc["from_branch"]} to {doc["to_branch"]} on {doc["timestamp"]}')
+        print(f'{doc["author"]} submitted a push request from {doc["from_branch"]} to {doc["to_branch"]} on {doc["timestamp"]}')
+        insert_into_mongodb(doc)
         return 'PUSH request event received', 200
         
     elif event_type == 'pull_request':
@@ -38,9 +44,10 @@ def github_webhook():
             "action": "PULL_REQUEST",
             "from_branch" : pr["head"]["ref"],
             "to_branch" : pr["base"]["ref"],
-            "timestamp" : datetime.now(timezone.utc).strftime('%d %B %Y - %I:%M %p UTC')
+            "timestamp" : datetime.now(IST).strftime('%d %B %Y - %I:%M %p UTC')
             }
             print(f'{doc["author"]} submitted a pull request from {doc["from_branch"]} to {doc["to_branch"]} on {doc["timestamp"]}')
+            insert_into_mongodb(doc)
             return 'Pull request event received', 200
         
         #MERGE request
@@ -51,17 +58,16 @@ def github_webhook():
             "action": "MERGE",
             "from_branch" : pr["head"]["ref"],
             "to_branch" : pr["base"]["ref"],
-            "timestamp" : datetime.now(timezone.utc).strftime('%d %B %Y - %I:%M %p UTC')
+            "timestamp" : datetime.now(IST).strftime('%d %B %Y - %I:%M %p UTC')
             }
-            print(f'{doc["author"]} merged branch {doc["from_branch"]} to {doc["to_branch"]} on {timestamp}')
+            print(f'{doc["author"]} merged branch {doc["from_branch"]} to {doc["to_branch"]} on {doc["timestamp"]}')
+            insert_into_mongodb(doc)
             return 'Pull request merged event received', 200
         else:
-            return f"pull request action {action} ignored", 200
-    # else:
-    #     return jsonify({"status": "unhandled event"}), 200
-    #insert data into mongodb
-    collection.insert_one(doc)
-    return jsonify({"status": "success"}), 200
+            return f"pull request action {doc["action"]} ignored", 200
+    else:
+        return jsonify({"status": "unhandled event"}), 200
+    
 
         
 if __name__ == '__main__':
